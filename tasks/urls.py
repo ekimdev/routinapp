a2ebecbedff7ddf78b9b4bdfccf45139efb424ba
Rodrigo Acosta <rodrigoacosta444@gmail.com>
2022-08-22 12:56:55 -0300

    feat: add CRUD to tasks
    C: add create_task view
    R: add task view
    U: add update_task view
    D: add delete_task
    and add delete_template, task_template

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 
 urlpatterns = [
-        path("", views.tasks, name="name"),
+        path("", views.tasks, name="tasks"),
+        path("task/<str:pk>/", views.task, name="single_task"),
+
+        path("create_task/", views.create_task, name="create_task"),
+        path("update_task/<str:pk>/", views.update_task, name="update_task"),
+        path("delete_task/<str:pk>", views.delete_task, name="delete_task"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Task
+from .forms import TaskForm
 
 
 def tasks(request):
@@ -8,3 +9,49 @@ def tasks(request):
 
     context = {"tasks": tasks}
     return render(request, "tasks/tasks.html", context)
+
+
+def task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    context = {"task": task}
+    return render(request, "tasks/single_task.html", context)
+
+
+def create_task(request):
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.save()
+            return redirect("tasks")
+
+    context = {"form": form}
+    return render(request, "tasks/task_form.html", context)
+
+
+def update_task(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("tasks")
+
+    context = {"form": form}
+    return render(request, "tasks/task_form.html", context)
+
+
+def delete_task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    if request.method == "POST":
+        task.delete()
+        return redirect("tasks")
+
+    context = {"object": task}
+    return render(request, "delete_template.html", context)
