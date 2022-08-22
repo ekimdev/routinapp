b9f4165e90186ad3bb57fa0e72a6cc90837d52c4
Rodrigo Acosta <rodrigoacosta444@gmail.com>
2022-08-22 09:35:46 -0300

    feat: add Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,14 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=500, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False, blank=True)
+
+    def __str__(self):
+        return self.title
