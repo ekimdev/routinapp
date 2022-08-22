b43d43d22f3ce04924db8706de82346ac80f7592
Rodrigo Acosta <rodrigoacosta444@gmail.com>
2022-08-22 14:58:06 -0300

    feat: add Tag model

--- tasks/forms.py
+++ tasks/forms.py
@@ -7,4 +7,7 @@ from .models import Task
 class TaskForm(ModelForm):
     class Meta:
         model = Task
-        fields = ["title", "description"]
+        fields = ["title", "description", "tags"]
+        widgets = {
+                "tags": forms.CheckboxSelectMultiple(),
+                }
--- tasks/models.py
+++ tasks/models.py
@@ -7,8 +7,18 @@ class Task(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=500, blank=True)
+    tags = models.ManyToManyField("Tag", blank=True)
     created = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False, blank=True)
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
