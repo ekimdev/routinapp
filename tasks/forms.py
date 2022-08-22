658f8e869b5bafba862b38044510835b24253188
Rodrigo Acosta <rodrigoacosta444@gmail.com>
2022-08-22 12:56:42 -0300

    feat: add task_form

---
+++ tasks/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from django import forms
+
+from .models import Task
+
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ["title", "description"]
